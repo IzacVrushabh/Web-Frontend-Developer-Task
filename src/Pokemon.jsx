d6607ce24b354e9fd8f523a3d5e5707e29b2528a
IzacVrushabh <vrushabhkulye@gmail.com>
2020-10-23 20:10:03 +0530

    Added comments

--- src/Pokemon.jsx
+++ src/Pokemon.jsx
@@ -19,8 +19,9 @@ const Pokemon = (props) => {
     sort: "",
   });
 
-  useEffect(() => {
+  //works on array..
 
+  useEffect(() => {
     setLoading(true);
     axios.get(`${base_url}50`).then((response) => {
       const { data } = response;
@@ -51,8 +52,8 @@ const Pokemon = (props) => {
       });
       console.log(details);
 
-      // const cnt = details.length;
-      // console.log(cnt);
+      const cnt = details.length;
+      console.log(cnt);
       setLoading(false);
     });
   }, []);
@@ -61,7 +62,7 @@ const Pokemon = (props) => {
   const sortPokemon = (event) => {
     const sort = event.target.value;
     // console.log(event.target.value);
-    // 
+    //
     setPokemonData((val) => ({
       sort: sort,
       details: val.details
@@ -84,6 +85,7 @@ const Pokemon = (props) => {
   };
 
   //Need proper implementation
+  //works on implemenrstation
   const filterPokemon = (event) => {
     console.log(event.target.value);
     if (event.target.value === "") {
@@ -105,110 +107,116 @@ const Pokemon = (props) => {
 
   return (
     <>
-      <AppBar
-        className="mt-2"
-        position="sticky"
-        style={{ borderRadius: "10px", background: "#FFB800" }}
-      >
-        <div className="nav_bar my-2 d-block mx-auto">
-          <h3>POKEMON API</h3>
-        </div>
-        <Toolbar>
-          <div>
-            <SearchIcon />
-            <TextField
-              className="Search"
-              onChange={handleSearchChange}
-              label="Pokemon"
-              variant="standard"
+      <div>
+        <AppBar
+          className="mt-2"
+          position="sticky"
+          style={{ borderRadius: "10px", background: "#FFB800" }}
+        >
+          <div className="nav_bar my-2 d-block mx-auto">
+            <h3>POKEMON API</h3>
+          </div>
+          <Toolbar>
+            <div>
+              <SearchIcon />
+              <TextField
+                className="Search"
+                onChange={handleSearchChange}
+                label="Pokemon"
+                variant="standard"
+              />
+            </div>
+          </Toolbar>
+          <div className="d-flex ">
+            <FilterSort
+              count={details.length}
+              filter_list={pokemonData.filter_list}
+              sort={pokemonData.sort}
+              sortPokemon={sortPokemon}
+              filterPokemon={filterPokemon}
             />
           </div>
-        </Toolbar>
-        <div className="d-flex ">
-          <FilterSort
-            count={details.length}
-            filter_list={pokemonData.filter_list}
-            sort={pokemonData.sort}
-            sortPokemon={sortPokemon}
-            filterPokemon={filterPokemon}
+        </AppBar>
+        {loading ? (
+          <Loader
+            type="ThreeDots"
+            color="#FFB800"
+            height="100"
+            width="100"
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
           />
-        </div>
-      </AppBar>
-      {loading ? (
-        <Loader
-          type="ThreeDots"
-          color="#FFB800"
-          height="100"
-          width="100"
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        />
-      ) : (
-        <table className="mt-3 d-block mx-auto" style={{ width: "100vh" }}>
-          <tbody>
-            {details.map((item) => {
-              const sprite = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${item.id}.png`;
-              // const { id, name, sprite } = item;
-              return (
-                <tr key={item.id} className="d-block">
-                  <td>
-                    <div
-                      className="card mb-3"
-                      style={{ maxWidth: "100vw", background: "#FFF8DE" }}
-                    >
-                      <div className="row no-gutters">
-                        <div
-                          className="col-md-4 my-3"
-                          style={{ borderRight: "5px solid #FFB800" }}
-                        >
-                          <img src={sprite} className="card-img" alt={sprite} />
-                        </div>
-                        <div className="col-md-8">
-                          <div className="card-body mt-2">
-                            <h5
-                              className="card-title"
-                              style={{ textTransform: "uppercase" }}
-                            >{`${item.id} - ${item.name}`}</h5>
-                            <p className="card-text">
-                              <b
+        ) : (
+          <table className="mt-3 d-block mx-auto" style={{ width: "100vh" }}>
+            <tbody>
+              {details.map((item) => {
+                const sprite = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${item.id}.png`;
+                // const { id, name, sprite } = item;
+                return (
+                  <tr key={item.id} className="d-block">
+                    <td>
+                      <div
+                        className="card mb-3"
+                        style={{ maxWidth: "100vw", background: "#FFF8DE" }}
+                      >
+                        <div className="row no-gutters">
+                          <div
+                            className="col-md-4 my-3"
+                            style={{ borderRight: "5px solid #FFB800" }}
+                          >
+                            <img
+                              src={sprite}
+                              className="card-img"
+                              alt={sprite}
+                            />
+                          </div>
+                          <div className="col-md-8">
+                            <div className="card-body mt-2">
+                              <h5
+                                className="card-title"
                                 style={{ textTransform: "uppercase" }}
-                              >{`${item.name}`}</b>
-                              Pokemon Description
-                            </p>
-                            <p className="card-text">
-                              <button
-                                type="button"
-                                className="btn btn-warning w-25 text-white font-weight-bold "
-                                onClick={() => history.push(`/${item.id}`)}
-                              >
-                                View
-                              </button>
-                              <button
-                                type="button"
-                                className="btn btn-warning w-25 text-white ml-3"
-                                onClick={() => {
-                                  setDetails((curCard) =>
-                                    curCard.filter((x) => x.id !== item.id)
-                                  );
-                                }}
-                              >
-                                <DeleteIcon />
-                              </button>
-                            </p>
+                              >{`${item.id} - ${item.name}`}</h5>
+                              <p className="card-text">
+                                <b
+                                  style={{ textTransform: "uppercase" }}
+                                >{`${item.name}`}</b>
+                                Pokemon Description
+                              </p>
+                              <p className="card-text">
+                                <button
+                                  type="button"
+                                  className="btn btn-warning w-25 text-white font-weight-bold "
+                                  onClick={() => history.push(`/${item.id}`)}
+                                >
+                                  View
+                                </button>
+                                <button
+                                  type="button"
+                                  className="btn btn-warning w-25 text-white ml-3"
+                                  onClick={() => {
+                                    setDetails((curCard) =>
+                                      curCard.filter((x) => x.id !== item.id)
+                                    );
+                                  }}
+                                >
+                                  <DeleteIcon />
+                                </button>
+                              </p>
+                            </div>
                           </div>
                         </div>
                       </div>
-                    </div>
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      )}
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        )}
+      </div>
     </>
   );
 };
