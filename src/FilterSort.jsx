672e8e5d081ce21b029aac4847cb82c20280f059
IzacVrushabh <vrushabhkulye@gmail.com>
2020-10-31 20:18:35 +0530

    Coreecting some code

--- src/FilterSort.jsx
+++ src/FilterSort.jsx
@@ -9,7 +9,12 @@ const FilterSort = (props) => {
       <div className="col-md-4 w-50">
         <label className="d-flex">
           Sort by
-          <select value={props.sort} onChange={props.sortPokemon} className="ml-2">
+          {/* //pass proper values.. */}
+          <select
+            value={props.sort}
+            onChange={props.sortPokemon}
+            className="ml-2"
+          >
             <option value="">Select</option>
             <option value="lowest">Lowest to highest</option>
             <option value="highest">Highest to lowest</option>
@@ -19,8 +24,12 @@ const FilterSort = (props) => {
 
       <div className="col-md-4 w-50 ml-3">
         <label className="d-flex">
-        Filter
-          <select value={props.filter_list} onChange={props.filterPokemon} className="ml-2">
+          Filter
+          <select
+            value={props.filter_list}
+            onChange={props.filterPokemon}
+            className="ml-2"
+          >
             <option value="">ALL</option>
             <option value="height">Height</option>
             <option value="types">Types</option>
@@ -28,7 +37,9 @@ const FilterSort = (props) => {
           </select>
         </label>
       </div>
-      <div className="filter-result"><h3> {props.count} Pokemons</h3></div>
+      <div className="filter-result">
+        <h3> {props.count} Pokemons</h3>
+      </div>
     </>
   );
 };
