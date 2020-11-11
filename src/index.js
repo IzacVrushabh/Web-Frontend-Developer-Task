e88ce49c6df281af86c0bcf0d23ba821a39f588c
IzacVrushabh <vrushabhkulye@gmail.com>
2020-11-11 20:38:34 +0530

    backend api

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import "./App.css";
 //use sass
+//backend implementation
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
