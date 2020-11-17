1c5b89d22fd7e35457eed4469e5c8d24ce6464ad
IzacVrushabh <vrushabhkulye@gmail.com>
2020-11-17 20:10:56 +0530

    redux commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import "./index.css";
 import "./App.css";
 //use sass
 //backend implementation with apis
+//use store from redux here
+// add api folder
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
