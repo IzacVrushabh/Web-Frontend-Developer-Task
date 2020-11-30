51abcf82d93d5750f0fd05b82d03785ed31237c4
IzacVrushabh <vrushabhkulye@gmail.com>
2020-11-30 20:15:12 +0530

    f2 commit

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import "./App.css";
 import App from "./App";
-//use sass
+//todo
+//use sass/scss
 //backend implementation with apis
 //use store from redux here
 // add api folder
 //initialise redux with redux tool
-
+//use react chart
 ReactDOM.render(<App />, document.getElementById("root"));
